023cabee50b71627720009a3af6f1028b1ba658b
trickerer <onlysuffering@gmail.com>
2024-05-04 12:36:49 +0700

    mkdir fix 1

--- update-merge_ac.sh
+++ update-merge_ac.sh
@@ -22,7 +22,7 @@ git status
 # install
 mysql -uroot -proot -e "SET PASSWORD FOR root@localhost='';"
 mysql -uroot -e 'create database test_mysql;'
-mkdir bin
+mkdir -p bin
 cd bin
 cmake ../ -DWITH_WARNINGS=1 -DWITH_COREDEBUG=0 -DUSE_COREPCH=1 -DUSE_SCRIPTPCH=1 -DTOOLS=1 -DSCRIPTS=dynamic -DSERVERS=1 -DNOJEM=0 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS_DEBUG="-DNDEBUG" -DCMAKE_CXX_FLAGS_DEBUG="-DNDEBUG" -DCMAKE_INSTALL_PREFIX=check_install
 cd ..
