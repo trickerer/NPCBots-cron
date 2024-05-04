eba1a3da187b84107e23fd0f8e48d90ede7d96ef
trickerer <onlysuffering@gmail.com>
2024-05-04 15:32:46 +0700

    Install AC data for dry run

--- update-merge_ac.sh
+++ update-merge_ac.sh
@@ -6,6 +6,7 @@ set -e
 # before install
 git clone --branch=${BRANCH} https://github.com/trickerer/AzerothCore-wotlk-with-NPCBots.git server
 cd server
+git clone --depth 1 --branch master --single-branch https://github.com/ac-data/ac-data.git check_install/data
 git config user.email "github.actions@build.bot" && git config user.name "Github Actions"
 git status
 if [ -n "$BASE_BRANCH" ]; then
@@ -24,7 +25,7 @@ mysql -uroot -proot -e "SET PASSWORD FOR root@localhost='';"
 mysql -uroot -e 'create database test_mysql;'
 mkdir -p bin
 cd bin
-cmake ../ -DWITH_WARNINGS=1 -DWITH_COREDEBUG=0 -DUSE_COREPCH=1 -DUSE_SCRIPTPCH=1 -DTOOLS=1 -DSCRIPTS=static -DSERVERS=1 -DNOJEM=0 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS_DEBUG="-DNDEBUG" -DCMAKE_CXX_FLAGS_DEBUG="-DNDEBUG" -DCMAKE_INSTALL_PREFIX=check_install
+cmake ../ -DWITH_WARNINGS=1 -DWITH_COREDEBUG=0 -DUSE_COREPCH=1 -DUSE_SCRIPTPCH=1 -DTOOLS=1 -DSCRIPTS=static -DMODULES=static -DSERVERS=1 -DBUILD_TESTING=ON -DNOJEM=0 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS_DEBUG="-DNDEBUG" -DCMAKE_CXX_FLAGS_DEBUG="-DNDEBUG" -DCMAKE_INSTALL_PREFIX=check_install
 cd ..
 
 # script
@@ -40,8 +41,6 @@ cp worldserver.conf.dist worldserver.conf
 cd ../bin
 ./authserver --dry-run
 ./worldserver --dry-run
-./authserver --version
-./worldserver --version
 
 # after success
 git push https://${GITHUB_TOKEN}@github.com/trickerer/AzerothCore-wotlk-with-NPCBots.git HEAD:${BRANCH}
