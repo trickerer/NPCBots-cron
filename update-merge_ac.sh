5bd407232a9a957da24e991c29eff62259f951cc
trickerer <onlysuffering@gmail.com>
2024-05-04 16:55:16 +0700

    Do not use existing `bin` folder for AC autobuild

--- update-merge_ac.sh
+++ update-merge_ac.sh
@@ -6,7 +6,7 @@ set -e
 # before install
 git clone --branch=${BRANCH} https://github.com/trickerer/AzerothCore-wotlk-with-NPCBots.git server
 cd server
-git clone --depth 1 --branch master --single-branch https://github.com/ac-data/ac-data.git bin/check_install/bin
+git clone --depth 1 --branch master --single-branch https://github.com/ac-data/ac-data.git build/check_install/bin
 git config user.email "github.actions@build.bot" && git config user.name "Github Actions"
 git status
 if [ -n "$BASE_BRANCH" ]; then
@@ -24,7 +24,7 @@ git status
 mysql -uroot -proot -e "SET PASSWORD FOR root@localhost='';"
 mysql -uroot -e 'create database test_mysql;'
 mkdir -p bin
-cd bin
+cd build
 cmake ../ -DWITH_WARNINGS=1 -DWITH_COREDEBUG=0 -DUSE_COREPCH=1 -DUSE_SCRIPTPCH=1 -DTOOLS=1 -DSCRIPTS=static -DMODULES=static -DSERVERS=1 -DBUILD_TESTING=ON -DNOJEM=0 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS_DEBUG="-DNDEBUG" -DCMAKE_CXX_FLAGS_DEBUG="-DNDEBUG" -DCMAKE_INSTALL_PREFIX=check_install
 cd ..
 
@@ -33,7 +33,7 @@ c++ --version
 mysql -uroot < data/sql/create/create_mysql.sql
 mysql -uroot < data/sql/create/drop_mysql_8.sql
 mysql -uroot < data/sql/create/create_mysql.sql
-cd bin
+cd build
 make -j 4 -k && make install && make clean
 cd check_install/etc
 cp authserver.conf.dist authserver.conf
