a37a5d61b6360c08934d7087de844ec1e779508f
trickerer <onlysuffering@gmail.com>
2026-07-18 09:18:46 +0700

    TC: update 1

--- update-merge.sh
+++ update-merge.sh
@@ -20,27 +20,15 @@ git submodule update --init --recursive
 git status
 
 # install
-mysql -uroot -proot -e "SET PASSWORD FOR root@localhost='';"
-mysql -uroot -e 'create database test_mysql;'
-mkdir bin
-cd bin
-cmake ../ -DWITH_WARNINGS=1 -DWITH_COREDEBUG=0 -DUSE_COREPCH=1 -DUSE_SCRIPTPCH=1 -DTOOLS=1 -DSCRIPTS=dynamic -DSERVERS=1 -DNOJEM=0 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS_DEBUG="-DNDEBUG" -DCMAKE_CXX_FLAGS_DEBUG="-DNDEBUG" -DCMAKE_INSTALL_PREFIX=check_install
-cd ..
-chmod +x contrib/check_updates.sh
+cmake -GNinja -S $WORKSPACE -B $OUTPUT_DIR -DWITH_WARNINGS=1 -DWITH_COREDEBUG=0 -DUSE_COREPCH=1 -DUSE_SCRIPTPCH=1 -DTOOLS=0 -DSCRIPTS=dynamic -DSERVERS=1 -DNOJEM=0 -DCMAKE_C_FLAGS_DEBUG="-DNDEBUG -g0" -DCMAKE_CXX_FLAGS_DEBUG="-DNDEBUG -g0" -DCMAKE_INSTALL_PREFIX=check_install -DBUILD_TESTING=0
 
 # script
-c++ --version
-mysql -uroot < sql/create/create_mysql.sql
-mysql -utrinity -ptrinity auth < sql/base/auth_database.sql
-./contrib/check_updates.sh auth 3.3.5 auth localhost
-mysql -utrinity -ptrinity characters < sql/base/characters_database.sql
-./contrib/check_updates.sh characters 3.3.5 characters localhost
-mysql -utrinity -ptrinity world < sql/base/dev/world_database.sql
-cat sql/updates/world/3.3.5/*.sql | mysql -utrinity -ptrinity world
-mysql -uroot < sql/create/drop_mysql_8.sql
-cd bin
-make -j 4 -k && make install && make clean
-cd check_install/bin
+ccache -z
+cmake --build $OUTPUT_DIR
+ccache -s
+ccache --evict-older-than $(($EPOCHSECONDS - $BUILD_START))s
+cmake --install $OUTPUT_DIR
+cd $INSTALL_DIR
 ./authserver --version
 ./worldserver --version
 
